002828f3333deaf8d1139322fea73a485987b503
James Ferrell <jamesferrell@jamess-mbp.lan>
2021-06-12 18:13:14 -0400

    skeleton built out, styling begun

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,9 @@
+export default function IndexPage() {
+  return (
+    <>
+      <p>hello</p>
+      <code>var</code>
+      <p>i am child</p>
+    </>
+  )
+}
---
+++ sick-fits/frontend/pages/orders.js
@@ -0,0 +1,5 @@
+export default function OrderPage() {
+  return <div>
+    <h1>hello world</h1>
+  </div>
+}
