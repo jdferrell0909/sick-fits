2ffd6a7c8f335b98e2e0bef302485d57ffe2a7a5
James Ferrell <jamesferrell@jamess-mbp.lan>
2021-06-15 11:42:20 -0400

    User and Product schemas created

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,31 +1,55 @@
-import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema';
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { User } from "./schemas/User";
+import { Product } from "./schemas/Product";
+import { createAuth } from "@keystone-next/auth";
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
 
-const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
 
 const sessionConfig = {
   maxAge: 60 * 60 * 24 * 360, //how long to stay signed in
   secret: process.env.COOKIE_SECRET,
-}
+};
 
-export default config({
-  server: {
-    cors: {
-      origin: [process.env.FRONTEND_URL],
-      credentials: true,
-    }
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    //TODO add in initial roles
   },
-  db: {
-    adapter: 'mongoose',
-    url: databaseURL,
-    // DATA SEEDING
-  },
-  lists: createSchema({
-    // schema items here
-  }),
-  ui: {
-    // todo change for roles
-    isAccessAllowed: () => true
-  },
-  // add session values here
-})
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      // DATA SEEDING
+    },
+    lists: createSchema({
+      // schema items here
+      User,
+      Product,
+    }),
+    ui: {
+      // show UI only for people who pass
+      isAccessAllowed: ({ session }) => {
+        // console.log(session);
+        return session?.data
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id name email`
+    })
+  })
+);
