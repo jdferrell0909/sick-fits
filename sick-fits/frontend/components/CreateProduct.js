28aec32cc90dccba487390663c6372048e18652b
James Ferrell <jamesferrell@Jamess-MacBook-Pro.local>
2021-06-17 18:57:02 -0400

    gql mutation for form set up and functioning correctly

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,5 +1,33 @@
 import useForm from '../lib/useForm';
 import Form from './styles/Form';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/client';
+import DisplayError from './ErrorMessage';
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    #which variables are getting passed in and what types
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        price: $price
+        status: "AVAILABLE"
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, clearForm, resetForm } = useForm({
@@ -9,14 +37,24 @@ export default function CreateProduct() {
     description: 'these are some nice shoes',
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
+
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        // submit input fields to backend
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset aria-busy>
+      <DisplayError error={error} />
+      <fieldset aria-busy={loading} disabled={loading}>
         <label htmlFor="name">
           Image
           <input
